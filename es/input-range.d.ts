db2f068e00deac67937187ad086e63c7d1c800e6
Pranesh Ravi <praneshr@indix.com>
2018-02-25 21:47:36 +0530

    Removing cleanup

--- es/input-range.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react'
-import { IFormElementProps } from './types'
-declare const InputRange: React.SFC<IFormElementProps>
-export default InputRange
--- lib/input-toggle.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react'
-import { IFormElementProps } from './types'
-declare const InputToggle: React.SFC<IFormElementProps>
-export default InputToggle
