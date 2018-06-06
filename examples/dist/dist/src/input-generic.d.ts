1ed09102b794283812b9af855d7762dc4907cb71
Pranesh Ravi <praneshpranesh@gmail.com>
2018-06-06 12:47:07 +0530

    Remove unwanted example files

--- examples/dist/dist/src/input-generic.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-import { IFormElementProps } from './types';
-declare const InputGeneric: React.SFC<IFormElementProps>;
-export default InputGeneric;
