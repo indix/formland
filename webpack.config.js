85d92f7a74f72ca9262069df99dcb59d1f9d8be7
Pranesh Ravi <praneshpranesh@gmail.com>
2018-06-06 14:11:03 +0530

    New tsconfig for examples

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const MinifyPlugin = require("babel-minify-webpack-plugin")
+const MinifyPlugin = require('babel-minify-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -14,9 +14,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'examples/dist'),
     filename: '[name].js',
-    publicPath: '',
   },
-  devServer : {
+  devServer: {
     contentBase: path.resolve(__dirname, 'examples/dist'),
     port: 8000,
     hot: true,
@@ -25,17 +24,21 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.examples.json',
+            },
+          },
+        ],
         exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: [
-            'css-loader?minimize=true',
-            'sass-loader',
-          ],
+          use: ['css-loader?minimize=true', 'sass-loader'],
         }),
       },
     ],
@@ -49,17 +52,15 @@ module.exports = {
       filename: 'common.js',
       minChunk: 2,
     }),
-    new ExtractTextPlugin('macin.css'),
+    new ExtractTextPlugin('main.css'),
   ].concat(
     process.env.NODE_ENV === 'production'
       ? [
-        new MinifyPlugin(),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('production')
-        })
-      ]
-      : [
-        new webpack.HotModuleReplacementPlugin(),
-      ]
+          new MinifyPlugin(),
+          new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+          }),
+        ]
+      : [new webpack.HotModuleReplacementPlugin()],
   ),
 }
